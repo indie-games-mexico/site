520a9632e0058d19aeb3624afd675ed60981fa6e
Ricardo Ibarra <coder10@gmail.com>
2019-04-06 07:22:16 -0700

    pending 404

--- src/pages/404.en.js
+++ src/pages/404.en.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React from "react";
+import SEO from "../components/common/seo";
+import styled from 'styled-components';
+import './404.css';
 
-import Layout from "../layouts/en";
-import SEO from "../components/common/seo"
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  Background-color: #ccc;
+`;
 
 const NotFoundPage = (props) => (
-  <Layout location={ props.location }>
+  <Background>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  </Background>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
