26f31870f2c163e270fd8a7aaebdaf8a9b5c859a
Ricardo Ibarra <coder10@gmail.com>
2019-05-21 23:04:33 -0700

    fixed 404 pgaes

--- src/pages/404.en.js
+++ src/pages/404.en.js
@@ -1,14 +1,40 @@
 import React from "react"
-
+import styled from 'styled-components';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Layout from "../layouts/en";
 import SEO from "../components/common/seo"
+import { JustifiedP, H2} from '../components/common/elements';
+
+const LayoutWrapper = styled.div`
+  background-color: #000;
+`;
+
+const ContentWrapper = styled.div`
+	color: #fff;
+	padding:200px 0 0 0;
+	font-size: 2em;
+`;
 
 const NotFoundPage = (props) => (
-  <Layout location={ props.location }>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <LayoutWrapper>
+	<Layout location={ props.location }>
+		<SEO title="404: Not found" />
+		<ContentWrapper>
+		<Grid>
+			<Row>
+				<Col md={12}>
+					<JustifiedP>You seem lost, why not try any of the links above?</JustifiedP>
+				</Col>
+			</Row>
+			<Row>
+				<Col md="12">
+					<H2 style={{ fontSize: '5em' }}>404</H2>
+				</Col>
+			</Row>
+		</Grid>
+		</ContentWrapper>
+	</Layout>
+  </LayoutWrapper>
 )
 
 export default NotFoundPage
--- src/pages/404.es.js
+++ src/pages/404.es.js
@@ -1,14 +1,40 @@
 import React from "react"
-
+import styled from 'styled-components';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Layout from "../layouts/es";
 import SEO from "../components/common/seo"
+import { JustifiedP, H2} from '../components/common/elements';
+
+const LayoutWrapper = styled.div`
+  background-color: #000;
+`;
+
+const ContentWrapper = styled.div`
+	color: #fff;
+	padding:200px 0 0 0;
+	font-size: 2em;
+`;
 
 const NotFoundPage = (props) => (
-  <Layout location={ props.location }>
-    <SEO title="404: No encontrado" />
-    <h1>No Encontrado</h1>
-    <p>El recurso que intenta acceder no existe</p>
-  </Layout>
+  <LayoutWrapper>
+	<Layout location={ props.location }>
+		<SEO title="404: No encontrado" />
+		<ContentWrapper>
+		<Grid>
+			<Row>
+				<Col md={12}>
+					<JustifiedP>Pareces perdido, Intenta acceder alguno de los links del menu principal</JustifiedP>
+				</Col>
+			</Row>
+			<Row>
+				<Col md="12">
+					<H2 style={{ fontSize: '5em' }}>404</H2>
+				</Col>
+			</Row>
+		</Grid>
+		</ContentWrapper>
+	</Layout>
+  </LayoutWrapper>
 )
 
 export default NotFoundPage
